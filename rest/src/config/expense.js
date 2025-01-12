9ab7f4693dd3a1ea7776e47aadec85f6c0dbc390
Sumit Bhardwaj <scrbwrm@gmail.com>
2025-01-13 03:01:49 +0530

    config

---
+++ rest/src/config/expense.js
@@ -0,0 +1,11 @@
+// TODO: Add more categories
+const EXPENSE_CATEGORY = {
+    HOME: 'home'
+}
+
+const expenseCategories = Object.values(EXPENSE_CATEGORY) 
+
+module.exports = {
+    EXPENSE_CATEGORY,
+    expenseCategories
+}
