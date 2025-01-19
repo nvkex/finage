4964b5821f27d3986531a7dd00af2b281b8110a7
Sumit Bhardwaj <scrbwrm@gmail.com>
2025-01-19 13:39:22 +0530

    button update

--- src/components/Atomic/Button.tsx
+++ src/components/Atomic/Button.tsx
@@ -1,10 +1,10 @@
-
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
+    className?: string;
 }
 
-export default function Button({ children }: ButtonProps) {
+export default function Button({ children, className = "", ...btnProps }: ButtonProps) {
     return (
-        <button className="px-8 py-2 text-sm bg-slate-800 text-slate-300 ">{children}</button>
+        <button className={`px-8 py-2 text-sm bg-slate-800 text-slate-300 ${className}`} {...btnProps} >{children}</button>
     );
 }
